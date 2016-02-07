23ce08d0fc75013affd4b74c564e09ae47dee9ea
masoumeh <m.seydi@uni-leipzig.de>
2016-02-07 12:59:43 +0100

    new version of dijkstra (dijks3) with the data from cornu

--- Crawl/crawl.py
+++ Crawl/crawl.py
@@ -5,15 +5,20 @@ import os
 reload(sys)  
 sys.setdefaultencoding('utf8')
 
+faAlphs = ['%D8%A7','%D8%A8', '%D9%BE', '%D8%AA', '%D8%AB', '%D8%AC', '%DA%86', '%D8%AD', '%D8%AE'
+            , 'D8%AF','%D8%B0', '%D8%B1', '%D8%B2' '%DA%98', '%D8%B3' , '%D8%B4', '%D8%B5', '%D8%B6'
+            , '%D8%B7', '%D8%B8', '%D8%B9', '%D8%BA', '%D9%81', '%D9%82', '%DA%A9', '%DA%AF', '%D9%84'
+            , '%D9%85', '%D9%86', '%D9%88', '%D9%87', '%DB%8C' ]
+Nums = [1204,1018,264,981, 13, 250, 161, 308, 175, 425, 14, 466, 152, 6, 406, 335, 74, 29, 90, 11, 
+        289, 47, 269, 171, 202, 141, 51, 1201, 676, 236, 121, 45]
+nums = [2,2]
 FaAlphs = ['%D8%A7','%D8%A8']
-Nums = [1204,1019]
-nums = [1,0]
 cnt = 0
 for ch in FaAlphs:
   for n in range(0,nums[cnt]):
-    os.system(u'phantomjs save_page.js http://library.tebyan.net/fa/Browse/Alphabet?Alpha='+ch+'#PageIndex='+str(nums[cnt]-1)+' > page.html')
+    print("n " ,n, " ch: ", ch)
+    os.system(u'phantomjs save_page.js http://library.tebyan.net/fa/Browse/Alphabet?Alpha='+ch+'#PageIndex='+str(n)+' > page.html')
     print('page.html created')
-    cnt = cnt + 1
     data = open("page.html",'r').read()
     soup = BeautifulSoup(data, 'html.parser')
     arr = soup.find_all('a')
@@ -40,7 +45,7 @@ for ch in FaAlphs:
 	          #exit()
                 else:
                   main_title = ''
-                  for page in range(1,pageSize):
+                  for page in range(1,pageSize + 1):
                     os.system(u'phantomjs save_page.js http://library.tebyan.net'+bookURL[0:-1]+ str(page) +' > bookpage.html')
 	            print(u'phantomjs save_page.js http://library.tebyan.net'+bookURL[0:-1]+ str(page)  +' > bookpage.html')       
                     data3 = open("bookpage.html",'r').read()
@@ -55,4 +60,4 @@ for ch in FaAlphs:
 			  result = unicode.join(u'\n',map(unicode,art.contents))    
                           f2.write(result)
                           break
-	              #exit()
+  cnt = cnt +1
